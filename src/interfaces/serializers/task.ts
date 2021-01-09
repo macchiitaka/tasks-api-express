abf794738f8950f9d09f78905af34689fbf676a1
macchiitaka <zxcvbnmtacashi@gmail.com>
2021-01-10 02:30:47 +0900

    fix(serializers): fix updated_at

--- src/interfaces/serializers/task.ts
+++ src/interfaces/serializers/task.ts
@@ -6,7 +6,7 @@ export class TaskSerializer {
   public serialize = (data: TaskModel) => ({
     ...data,
     created_at: new Date(data.created_at).toISOString(),
-    updated_at: new Date(data.created_at).toISOString(),
+    updated_at: new Date(data.updated_at).toISOString(),
   })
 
   public serializeAll = (data: TaskModel[]) => ({
