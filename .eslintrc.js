62db358efa048265fc4d7639c54243bdb75a1aa2
Takashi Machinaga <zxcvbnmtacashi@gmail.com>
2021-12-06 00:30:46 +0900

    refactor(eslint): tweak config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,19 @@ module.exports = {
     node: true,
     'jest/globals': true,
   },
-  extends: [
-    '@takamachi/eslint-config/presets/node-typescript-prettier',
-    'plugin:jest/recommended',
-  ],
+  extends: ['@takamachi/eslint-config/presets/node-typescript-prettier'],
   parserOptions: {
     project: './tsconfig.json',
   },
+  overrides: [
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:jest/recommended'],
+      settings: {
+        jest: {
+          version: 27,
+        },
+      },
+    },
+  ],
 }
